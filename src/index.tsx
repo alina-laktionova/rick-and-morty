368fc549605d15ff8c3e82156a6a4bb91b038662
Alina <alinaa3333@gmail.com>
2022-05-27 21:21:53 +0300

    refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import {ThemeProvider} from "@mui/material";
 import {theme} from "./config/muiCustomTheme";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./store/store";
 
@@ -15,9 +15,9 @@ const root = ReactDOM.createRoot(
 root.render(
     <Provider store={store}>
         <ThemeProvider theme={theme}>
-            <BrowserRouter>
+            <HashRouter>
                 <App/>
-            </BrowserRouter>
+            </HashRouter>
         </ThemeProvider>
     </Provider>
 );
