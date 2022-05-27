250763f578f85eca49b0b028958486fed9d2bea7
Alina <alinaa3333@gmail.com>
2022-05-27 21:15:27 +0300

    refactoring

--- src/config/pathes.ts
+++ src/config/pathes.ts
@@ -5,5 +5,5 @@ export const GET_CHARACTERS_URL = BASE_URL+'character/'
 export const GET_EPISODES_URL = BASE_URL+'episode/'
 
 //routes
-export const CHARACTERS = '/characters'
-export const WATCH_LIST = '/my-watch-list'
+export const CHARACTERS = '/rick-and-morty/characters'
+export const WATCH_LIST = '/rick-and-morty/my-watch-list'
